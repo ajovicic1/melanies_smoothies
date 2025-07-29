3256dc1699b23d51bd778554597f9171b932d2da
ajovicic1 <ajovicic@live.com>
2025-07-29 04:09:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ if ingredients_list:
         st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
 # Nem section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 st.text(smoothiefroot_response.json())
